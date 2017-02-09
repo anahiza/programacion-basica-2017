e6e5f1cc173c9c7dda23a579b2d2ded291ffe9b5
Anhy <anahi.z.a@gmail.com>
2017-02-09 16:21:07 -0600

    Funciones en JavaScript

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,15 +1,21 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
+var inicio = 0;
+var final = 300;
 
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(100,100);
-lienzo.lineTo(200,200);
-lienzo.stroke();
-lienzo.closePath();
+for (i = 0; i <= 300; i+=10){
+    dibujarLinea(inicio, 0, 0, final, "black");
+    final-=10;
+    inicio+=10;
+}
 
-lienzo.beginPath();
-lienzo.fillRect(20,30,40,50);
-lienzo.stroke();
-lienzo.closePath();
+
+function dibujarLinea(x1, y1, x2, y2, color) {
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(x1,y1);
+  lienzo.lineTo(x2,y2);
+  lienzo.stroke();
+  lienzo.closePath();
+}
