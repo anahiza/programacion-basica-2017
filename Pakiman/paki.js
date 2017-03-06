7e0b6212181d3936df8af282032c2a53accfd845
Anahi <anahi.z.a@gmail.com>
2017-03-05 21:13:47 -0600

    Clases y Arrays parte 1

---
+++ Pakiman/paki.js
@@ -0,0 +1,15 @@
+class Pakiman {
+  constructor(n, v, a) {
+    this.nombre=n
+    this.vida = v
+    this.ataque=a
+  }
+
+  hablar(){
+
+  }
+}
+
+var cauchin = new Pakiman("Cauchin", 100, 30)
+var pokacho = new Pakiman("Pokacho", 80, 50)
+var tocinauro = new Pakiman("tocinauro", 120, 40)
