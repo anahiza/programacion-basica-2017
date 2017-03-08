d5e71ffd412d49de0c0ae409dc4884495038eee0
Anhy <anahi.z.a@gmail.com>
2017-03-08 16:06:34 -0600

    Clases y Arrays en JavaScript

--- Pakiman/paki.js
+++ Pakiman/paki.js
@@ -1,15 +1,13 @@
-class Pakiman {
-  constructor(n, v, a) {
-    this.nombre=n
-    this.vida = v
-    this.ataque=a
-  }
-
-  hablar(){
-
-  }
-}
 
 var cauchin = new Pakiman("Cauchin", 100, 30)
 var pokacho = new Pakiman("Pokacho", 80, 50)
-var tocinauro = new Pakiman("tocinauro", 120, 40)
+var tocinauro = new Pakiman("Tocinauro", 120, 40)
+
+var coleccion = []
+coleccion.push(cauchin)
+coleccion.push(pokacho)
+coleccion.push(tocinauro)
+
+for(var p of coleccion) {
+  p.mostrar()
+}
